fd97ad020bde2eb04aec15167cc13cfc6b3e2d78
priyanshu solanki <131973299+Priyanshu91930@users.noreply.github.com>
2026-06-19 13:29:14 +0530

    fix: update InputPeerPhotoFileLocation to use photo_id instead of deprecated volume_id/local_id (Telegram API Layer 146+)

--- TechVJ/utils/custom_dl.py
+++ TechVJ/utils/custom_dl.py
@@ -145,8 +145,7 @@ class ByteStreamer:
 
             location = raw.types.InputPeerPhotoFileLocation(
                 peer=peer,
-                volume_id=file_id.volume_id,
-                local_id=file_id.local_id,
+                photo_id=file_id.media_id,
                 big=file_id.thumbnail_source == ThumbnailSource.CHAT_PHOTO_BIG,
             )
         elif file_type == FileType.PHOTO:
